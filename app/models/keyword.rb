f9fbf57ab3eae28712d77c4148d4fde233c72a46
juafrlo <juafrlo@gmail.com>
2011-11-11 10:20:12 +0100

    Reverse last_30_measurements

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -3,6 +3,6 @@ class Keyword < ActiveRecord::Base
   has_one :latest_m, :class_name => 'Measurement', :order => 'measurements.id desc'
 
   def last_30_measurements
-    self.measurements.find(:all, :limit => 30, :order => 'id desc')
+    self.measurements.find(:all, :limit => 30, :order => 'id desc').reverse
   end
 end
