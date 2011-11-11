d929938b08bc7780516de17ed3b6bfc0381f1a90
juafrlo <juafrlo@gmail.com>
2011-11-11 10:03:30 +0100

    Updated to use only the last 30 measured to draw the graph

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,8 @@
 class Keyword < ActiveRecord::Base
   has_many :measurements
   has_one :latest_m, :class_name => 'Measurement', :order => 'measurements.id desc'
+
+  def last_30_measurements
+    self.measurements.find(:all, :limit => 30, :order => 'id desc')
+  end
 end
